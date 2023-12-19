b1c59b52c46d3b4786c401c43a0e093d9c21d420
Fher03 <Feransic@gmail.com>
2023-12-18 18:13:52 -0600

    Add files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom"; 
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function App() {
+  return <h1>One Piece Blog</h1>;
+};
+
+root.render(<App />);
